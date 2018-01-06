3b3250307992895f00de767a56e62e654c605fec
ankur.singh2 <ankur08admin@gmail.com>
2018-01-06 20:14:03 +0530

    initial commit

---
+++ view.js
@@ -0,0 +1,118 @@
+let $ = require('jquery')
+let fs = require('fs')
+let filename = 'contacts'
+let sno = 0
+let snod = 0
+var njds = require('nodejs-disks');
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+
+function execute(command, callback){
+    exec(command, function(error, stdout, stderr){ callback(stdout); });
+};
+$('#add-to-list').on('click', () => {
+   let name = $('#Name').val()
+   let email = $('#Email').val()
+
+   fs.appendFile('contacts', name + ',' + email + '\n')
+
+   addEntry(name, email)
+})
+
+function addEntry(name, email) {
+   if(name && email) {
+      sno++
+      let updateString = '<tr><td>'+ sno + '</td><td>'+ name +'</td><td>' 
+         + email +'</td></tr>'
+      $('#contact-table').append(updateString)
+   }
+}
+function addDriveEntry(name, size,freeSize) {
+   if(name && size && freeSize) {
+      snod++
+      let updateString = '<tr><td>'+ snod + '</td><td>'+ name +'</td><td>' 
+         + size +'</td></tr>'+ freeSize +'</td></tr>'
+      $('#drive-table').append(updateString)
+   }
+}
+function listDrives(){
+    njds.drives(
+        function (err, drives) {
+            njds.drivesDetail(
+                drives,
+                function (err, data) {
+                    for(var i = 0; i<data.length; i++)
+                    {
+						addDriveEntry(data[i].drive,data[i].total,data[i].freePer);
+						console.log("==================");
+                        /* Get drive mount point */
+                        console.log("Mount Point "+data[i].mountpoint);
+
+                        /* Get drive total space */
+                        console.log("Total Space "+data[i].total);
+
+                        /* Get drive used space */
+                        console.log("Used Space "+data[i].used);
+
+                        /* Get drive available space */
+                        console.log("Available Space "+data[i].available);
+
+                        /* Get drive name */
+                        console.log("Name "+data[i].drive);
+
+                        /* Get drive used percentage */
+                        console.log("Percentage used " +data[i].usedPer);
+
+                        /* Get drive free percentage */
+                        console.log("Free Percentage "+data[i].freePer);
+						
+						console.log("");
+						console.log("");
+                    }
+                }
+            );
+        }
+    )
+}
+function loadAndDisplayContacts() {  
+   
+   //Check if file exists
+   if(fs.existsSync(filename)) {
+      let data = fs.readFileSync(filename, 'utf8').split('\n')
+      
+      data.forEach((contact, index) => {
+         let [ name, email ] = contact.split(',')
+         addEntry(name, email)
+      })
+   
+   } else {
+      console.log("File Doesn\'t Exist. Creating new file.")
+      fs.writeFile(filename, '', (err) => {
+         if(err)
+            console.log(err)
+      })
+   }
+}
+function ping(){
+//     ls = spawn('ping',['google.com']);
+
+   ls = spawn('top')
+     ls.stdout.on('data', function (data) {
+        console.log('stdout: ' + data.toString());
+        $('#shell-output').text(data.toString());
+      });
+      
+      ls.stderr.on('data', function (data) {
+        console.log('stderr: ' + data.toString());
+        $('#shell-output').text(data.toString());
+      });
+      
+      ls.on('exit', function (code) {
+        console.log('child process exited with code');
+        $('#shell-output').text();
+      });
+}
+loadAndDisplayContacts()
+
+listDrives();
+ping();
