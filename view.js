d89dd78011893a5343668ccc9a11cc41feed286f
root <root@localhost.localdomain>
2018-01-12 15:10:32 -0500

    foremost

--- view.js
+++ view.js
@@ -4,7 +4,7 @@ let fs = require('fs')
 let filename = 'contacts'
 let sno = 0
 let snod = 0
-var njds = require('nodejs-disks');
+//var njds = require('nodejs-disks');
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 var sd;
@@ -71,7 +71,24 @@ function addDriveEntry(name, size, freeSize) {
         //     });
     }
 }
-function listDrives() {
+const df = require('@sindresorhus/df');
+function listDrives(){ 
+$('#drive-table').empty()
+df().then(list => {
+   
+   for(i=0;i<list.length;i++){
+d = list[i];
+	if(d.filesystem.startsWith("/dev")){
+	
+	 console.log(d.filesystem +
+		" size "+ d.size/(1024*1024*1024));
+ addDriveEntry(d.filesystem, d.size/(1024*1024*1024), "data[i].freePer");
+                writeOutput(d.device);
+	}
+	}
+});
+}
+function listDrives2() {
     $('#drive-table').empty()
     drivelist.list((error, drives) => {
         if (error) {
@@ -82,52 +99,12 @@ function listDrives() {
 
         for (i = 0; i < drives.length; i++) {
             d = drives[i];
-            if (!d.isSystem) {
+            //if (!d.isSystem) {
                 addDriveEntry(d.device, "data[i].total", "data[i].freePer");
                 writeOutput(d.device);
-            }
+            //}
         }
     });
-}
-function listDrives1() {
-    $('#drive-table').empty()
-    njds.drives(
-        function (err, drives) {
-            drv = drives
-            njds.drivesDetail(
-                drives,
-                function (err, data) {
-                    for (var i = 0; i < data.length; i++) {
-                        addDriveEntry(data[i].drive, data[i].total, data[i].freePer);
-                        writeOutput("==================");
-                        /* Get drive mount point */
-                        writeOutput("Mount Point " + data[i].mountpoint);
-
-                        /* Get drive total space */
-                        writeOutput("Total Space " + data[i].total);
-
-                        /* Get drive used space */
-                        writeOutput("Used Space " + data[i].used);
-
-                        /* Get drive available space */
-                        writeOutput("Available Space " + data[i].available);
-
-                        /* Get drive name */
-                        writeOutput("Name " + data[i].drive);
-
-                        /* Get drive used percentage */
-                        writeOutput("Percentage used " + data[i].usedPer);
-
-                        /* Get drive free percentage */
-                        writeOutput("Free Percentage " + data[i].freePer);
-
-                        writeOutput("");
-                        writeOutput("");
-                    }
-                }
-            );
-        }
-    )
 }
 
 // function loadAndDisplayContacts() {  
@@ -149,23 +126,75 @@ function listDrives1() {
 //       })
 //    }
 // }
+
 //DANGEROUS METHOD 
 function startCloaning(source, destination) {
     //DO NOT CHANGE IT CAN DAMAGE YOUR DISK
-    ls = spawn('dd', ['if=' + source, 'of=' + destination]);
+    //ls = spawn('dc3dd', ['if=' + source, 'of=' + destination]);
+    ls = spawn('dcfldd', ['if=' + source, 'of=' + destination]);
     ls.stdout.on('data', function (data) {
         writeOutput('stdout: ' + data.toString());
-        $('#shell-output').text(data.toString());
+        //writeOutput(data.toString());
     });
 
     ls.stderr.on('data', function (data) {
         writeOutput('stderr: ' + data.toString());
-        $('#shell-output').text(data.toString());
+        //writeOutput(data.toString());
     });
 
     ls.on('exit', function (code) {
-        writeOutput('child process exited with code');
-        $('#shell-output').text();
+        writeOutput('child process exited with code ' + code);
+	if(code==0){
+$('#sa').show();
+$('#sf').show();
+}
+    });
+}
+function startForeMostAnalysis(imagePath) {
+   var output = require('path').dirname(imagePath)+'/foremost-output';
+ try {
+    fs.mkdirSync(output)
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err
+  }
+    ls = spawn('foremost', ['-T','all','-v','-i',imagePath,'-o',output]);
+
+    ls.stdout.on('data', function (data) {
+        writeOutput('stdout: ' + data.toString());
+        //writeOutput(data.toString());
+    });
+
+    ls.stderr.on('data', function (data) {
+        writeOutput('stderr: ' + data.toString());
+        //writeOutput(data.toString());
+    });
+
+    ls.on('exit', function (code) {
+        writeOutput('child process exited with code ' + code);
+    });
+}
+
+function startAutopsyAnalysis(imagePath) {
+   var output = require('path').dirname(imagePath)+'/foremost-output';
+ try {
+    fs.mkdirSync(output)
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err
+  }
+    ls = spawn('foremost', ['-T','all','-v','-i',imagePath,'-o',output]);
+
+    ls.stdout.on('data', function (data) {
+        writeOutput('stdout: ' + data.toString());
+        //writeOutput(data.toString());
+    });
+
+    ls.stderr.on('data', function (data) {
+        writeOutput('stderr: ' + data.toString());
+        //writeOutput(data.toString());
+    });
+
+    ls.on('exit', function (code) {
+        writeOutput('child process exited with code ' + code);
     });
 }
 function ping() {
@@ -198,6 +227,8 @@ function getDestinationFromDrivePath(path) {
 function writeOutput(message){
     if(message){
          $('#shell-output').append(message+"</br>");
+   $('#shell-output').animate({scrollTop: $('#shell-output').prop("scrollHeight")}, 500);
+    
     }
 }
 
@@ -209,14 +240,20 @@ function showFileSaveDialog(){
         }
         destination = fileName;
         writeOutput('Source ' + sd + " destination file " + destination);
+$('#start').show();
        
     }); 
 }
 
 $(document).ready(function () {
+ $('#sf').hide();
+$('#destination').hide();
+$('#sa').hide();
+$('#start').hide();
     $('#drive-table').on('click', 'li', function () {
           var txt = $(this).text();
           sd = txt;
+$('#destination').show();
           writeOutput(txt);
     });
     $('#destination').on('click', () => {
@@ -229,6 +266,16 @@ $(document).ready(function () {
                 source = sd;
                 //destination = getDestinationFromDrivePath(sd)
                 startCloaning(source, destination);
+          }
+    })
+ $('#sf').on('click', () => {
+          if (destination) {
+	      startForeMostAnalysis(destination);
+          }
+    });
+ $('#sa').on('click', () => {
+          if (destination) {
+                startAutopsyAnalysis(destination);
           }
     })
 });
